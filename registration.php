71bfe2247c8d1ec43ca731a302d1493f4d11c53e
HỒ TRỌNG LINH ÂN <an.ho1991@gmail.com>
2016-08-01 11:39:50 +0700

    Change Name space

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Ubertheme_Ubdatamigration',
+    'WiseRobot_WiseDataMigration',
     __DIR__
 );
